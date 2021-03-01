849bf97db62445769ae1f62f82a1ecf29152e676
Edvaldo Leite <eguilhermeleite@gmail.com>
2021-03-01 00:49:47 -0300

    Entendendo o package.json e instalando o express
    Criando o package.json e instalando o express

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
-const http = require('http');
+const express = require('express');
+let app = express();
 const port = process.env.PORT || 3000;
 
-// recebe as requisições e as responde
-let server = http.createServer((req, res) => {
+// rota padrão
+app.get('/', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end("Hello!")
+});
 
-    // qual a url requisitada
-    console.log("URL: ", req.url);
-    // qual o metodo utilizado
-    console.log("MÉTODO: ", req.method);
+// rota users
+app.get('/users', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users: [{ 'nome': "Edvaldo Leite", 'email': 'eguilhermeleite@gmail.com' }, { 'nome': "Davih Leite", 'email': 'dguilhermesilvaleite@gmail.com' }
+        ]
+    });
 
-    switch (req.url) {
-        case "/":
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end("Hello!");
-            break
-
-        case "/users":
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({
-                users: [{
-                    'nome': "Edvaldo Leite", 'email': 'eguilhermeleite@gmail.com'
-                },
-                {
-                    'nome': "Davih Leite", 'email': 'dguilhermesilvaleite@gmail.com'
-                }
-                ]
-            }));
-            break
-    }
-})
+});
 
 // ouve a requisição
-server.listen(port, () => console.log(`SERVIDOR FUNCIONANDO NA PORTA ${port}. PRESSIONE CTRL + C PARA FINALIZAR.`))
+app.listen(port, () => console.log(`SERVIDOR FUNCIONANDO NA PORTA ${port}. PRESSIONE CTRL + C PARA FINALIZAR.`))
